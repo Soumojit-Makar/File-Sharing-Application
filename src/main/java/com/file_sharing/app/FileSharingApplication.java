21ffe29cfd64e8a7275db94a56efcb4f02d3fb83
soumo <soumojitmakar1234@gmail.com>
2025-02-17 20:42:12 +0530

    add all files

--- src/main/java/com/file_sharing/app/FileSharingApplication.java
+++ src/main/java/com/file_sharing/app/FileSharingApplication.java
@@ -1,10 +1,13 @@
 package com.file_sharing.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class FileSharingApplication {
+@EnableScheduling
+public class FileSharingApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileSharingApplication.class, args);
